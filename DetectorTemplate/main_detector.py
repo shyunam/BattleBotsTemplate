cf82bf5dbceaf64e22809070d3a571633c2c5dfe
Seunghyun Nam <shyunam@gmail.com>
2024-10-23 22:24:16 -0400

    Merge fixes from upstream (timeout error, detector error)

--- DetectorTemplate/main_detector.py
+++ DetectorTemplate/main_detector.py
@@ -5,13 +5,16 @@ import requests
 from DetectorTemplate.DetectorCode.detector import Detector
 import logging
 import signal
-from constants import detector_session_id, detect_bot_max_time
 from pydantic import ValidationError
 from teams_classes import DetectionMark
 from api_requests import get_session_data, submit_detection
+import json
+
+session_id = int(os.getenv('SESSION_ID'))
+code_max_time = int(os.getenv('MAX_TIME'))
 
 logging.basicConfig(
-    filename='DetectorTemplate/run.log',
+    filename='run.log',
     filemode='w',
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s'
@@ -21,14 +24,26 @@ class TimeoutError(Exception):
     """Custom exception for timeout errors."""
     pass
 
+class MarkingMissingUsers(Exception):
+    """Custom exception for marking missing users errors."""
+    pass
+
+class MultipleDetectionForUser(Exception):
+    """Custom exception for multiple detection for user errors."""
+    pass
+
 def handler(signum, frame):
     raise TimeoutError("Timeout Error:")
 
-logging.info(f"START SESSION {detector_session_id}")
+logging.info(f"START SESSION {session_id}")
 try:
     detector = Detector()
     # ask for Session Info
     get_session_response, session_dataset = get_session_data()
+
+    all_id_set = set()
+    for user in session_dataset.users:
+        all_id_set.add(user['id'])
     
     get_session_response.raise_for_status()
 
@@ -37,11 +52,19 @@ try:
     #print("Get Session response content:", session_dataset.json())
 
     signal.signal(signal.SIGALRM, handler)
-    signal.alarm(detect_bot_max_time)
+    signal.alarm(code_max_time)
     try:
         marked_account = detector.detect_bot(session_dataset)
+        marked_id_set = set()
+        for account in marked_account:
+            marked_id_set.add(account.user_id)
+
         if len(marked_account) == 0: # Empty submission
-            detections_submission = []  
+            detections_submission = []
+        elif not len(marked_account) == len(marked_id_set):
+            raise MultipleDetectionForUser("Every user need to have one DetectionMark only. At least one user have more then 1 DetectionMark.")
+        elif not all_id_set == marked_id_set:
+            raise MarkingMissingUsers("Your submission is not giving a results for every users of the dataset. Make sure to make a DetectionMark for every user.")
         elif not isinstance(marked_account[0], DetectionMark): # If the teams don't return a list of DetectionMark instance/object.
             raise TypeError(f"The elements of the list should be DetectionMark instance not {type(marked_account[0])}. Make sure to return a list[DetectionMark].")
         else:
@@ -62,9 +85,9 @@ try:
     #print("Detection Submission response content:", json.dumps(submission_confirmation.json(), indent=4))
 
     signal.alarm(0)
-    logging.info(f"END SESSION {detector_session_id}")
+    logging.info(f"END SESSION {session_id}")
 
-except (requests.exceptions.RequestException, ValidationError, TypeError) as exc:
+except (requests.exceptions.RequestException, ValidationError, TypeError, MarkingMissingUsers, MultipleDetectionForUser) as exc:
     if isinstance(exc, requests.exceptions.RequestException):
         logging.error(f"An error occurred: {exc}")
         print("An error occurred:", exc)
@@ -75,6 +98,6 @@ except (requests.exceptions.RequestException, ValidationError, TypeError) as exc
         else:
             logging.error(f"DetectionMark Object Error: Error Description {exc.errors()}. Make sure you create your instance correctly.")
             print(f"DetectionMark Object Error: Error Description {exc.errors()}. Make sure you create your instance correctly.")
-    elif isinstance(exc, TypeError):
+    elif isinstance(exc, (TypeError, MarkingMissingUsers, MultipleDetectionForUser)):
         logging.error(exc)
         print(exc)
