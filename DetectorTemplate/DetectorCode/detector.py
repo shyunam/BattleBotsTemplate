3f8af98c14b8b0f6452dbb12909a53ec7e250ba2
Seunghyun Nam <shyunam@gmail.com>
2024-11-08 04:09:27 -0500

    Added new grammar feature

--- DetectorTemplate/DetectorCode/detector.py
+++ DetectorTemplate/DetectorCode/detector.py
@@ -1,67 +1,77 @@
 from abc_classes import ADetector
 from teams_classes import DetectionMark
-from transformers import pipeline
-import torch
+import enchant
+import re
 
 '''
-METHOD: Sentiment Analysis
+METHOD: Grammar accuracy check
 '''
 
-classifier = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
-THRESHOLD = 0.95 # Min threshold for sentiment score for bot
+THRESHOLD = 0.01 # Min threshold for percentage of misspelled words 
 
 class Detector(ADetector):
 
     def detect_bot(self, session_data):
+        d = enchant.Dict("en_US")
         marked_account = []
-        user_scores = {} # Dictionary to track total sentiment scores per user
+        user_scores = {} # Dictionary to track number of misspelled and total word count
+        misspelled_words_by_bot = {} # for testing
+
+        # Check only lowercase alphabetic words
+        lowercase = re.compile(r'^[a-z]+$')
 
         for user in session_data.users:
-            user_scores[user['id']] = 0
-
-        # Sentiment analysis by post
+            user_scores[user['id']] = {'misspelled': 0, 'total': 0}
+            misspelled_words_by_bot[user['id']] = []
+        
+        # Count misspelled words
         for post in session_data.posts:
             user_id = post['author_id']
-            sentiment_result = classifier(post['text'], truncation=True)[0]
-            score = sentiment_result['score']
+            word_list = post['text'].split()
 
-            #print(user_id + ' ' + post['text'] + ' ' + str(score) + ' ' + sentiment_result['label'])
+            for word in word_list:
+                if lowercase.match(word):
+                    user_scores[user_id]['total'] += 1
+                    if not d.check(word):
+                        user_scores[user_id]['misspelled'] += 1
+                        misspelled_words_by_bot[user_id].append(word) # for testing
 
-            if user_id in user_scores and sentiment_result['label']!='NEU':
-                user_scores[user_id] += score
+            # If the first word of the post is not capitalized or is not an alphabet, consider it a misspell
+            if post['text']:
+                if not post['text'][0].isalpha() or post['text'][0].islower():
+                    user_scores[user_id]['total'] += 1
+                    user_scores[user_id]['misspelled'] += 1
 
         # Detect bots
         for user in session_data.users:
             user_id = user['id']
-            tweet_count = user['tweet_count']
-            total_score = user_scores[user_id]
+            misspelled = user_scores[user_id]['misspelled']
+            total_words = user_scores[user_id]['total']
             z_score = user['z_score']
 
-            average_score = 0
-            confidence = 0
-            is_bot = False
-
-            if tweet_count == 0:
-                average_score = THRESHOLD
-                confidence = 100
-                is_bot = True
-            else:
-                average_score = total_score/tweet_count
-                if average_score >= THRESHOLD:
-                    confidence = average_score*100
-                    is_bot = True
-                else: 
-                    confidence = (1-average_score)*100
+            misspelled_percentage = (misspelled / total_words) if total_words > 0 else 1
             
-            # Case where tweet count attribute is wrong
-            if confidence > 100:
-                confidence = 100
-                is_bot = True
+            is_bot = misspelled_percentage <= THRESHOLD
+            
+            if is_bot:
+                confidence = int((1-misspelled_percentage) * 100)
+            else:
+                if misspelled_percentage >= 0.1:
+                    confidence = 100
+                else:
+                    confidence = 50 + misspelled_percentage*100
+
             
             if z_score == 0:
                 is_bot = True
             
-            #print(user_id + ' ' + str(average_score))
             marked_account.append(DetectionMark(user_id=user_id, confidence=int(confidence), bot=is_bot))
+
+
+            # for testing
+            '''
+            if '-' in user_id:
+                print(f"User ID: {user_id} Words: {misspelled_words_by_bot[user_id]}")
+            '''
 
         return marked_account
--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,12 @@
-FROM python:3.8.12
+FROM python:3
 
 RUN pip install requests
 RUN pip install pydantic
-# Install Rust and Cargo (required by some transformers components)
-# Install curl to fetch the Rust installer
-#RUN apt-get update && apt-get install -y curl libsentencepiece-dev libssl-dev && \
-    #curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && \
-    #export PATH="/root/.cargo/bin:${PATH}" && \
-    #pip install 'transformers[torch]'
-RUN pip install transformers[torch] emoji==0.6.0
+
+RUN apt-get update && apt-get install -y \
+    libenchant-2-2  # Enchant library for pyenchant
+
+RUN pip install pyenchant
 
 #Important so we will have access to the run.sh file 
 COPY . . 
